16817a2d6b0bf0a12b31134d425a1085299fc364
Melissa LeBlanc-Williams <melissa@adafruit.com>
2022-07-22 15:48:51 -0700

    Update example with something that sort of works now

--- kb2040_sense_accelerometer_continuous/kb2040_sense_accelerometer_continuous.ino
+++ kb2040_sense_accelerometer_continuous/kb2040_sense_accelerometer_continuous.ino
@@ -1,5 +1,5 @@
 /* Edge Impulse Arduino examples
- * Copyright (c) 2021 EdgeImpulse Inc.
+ * Copyright (c) 2022 EdgeImpulse Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -24,56 +24,71 @@
 #include <Gesture-Hoodie_inferencing.h>
 #include <Adafruit_LSM6DS3TRC.h>
 
-/* Constant defines -------------------------------------------------------- */
-#define CONVERT_G_TO_MS2    9.80665f
-#define MAX_ACCEPTED_RANGE  2.0f        // starting 03/2022, models are generated setting range to +-2, but this example use Arudino library which set range to +-4g. If you are using an older model, ignore this value and use 4.0f instead
-
 Adafruit_LSM6DS3TRC lsm6ds3trc;
 
-/*
- ** NOTE: If you run into TFLite arena allocation issue.
- **
- ** This may be due to may dynamic memory fragmentation.
- ** Try defining "-DEI_CLASSIFIER_ALLOCATION_STATIC" in boards.local.txt (create
- ** if it doesn't exist) and copy this file to
- ** `<ARDUINO_CORE_INSTALL_PATH>/arduino/hardware/<mbed_core>/<core_version>/`.
- **
- ** See
- ** (https://support.arduino.cc/hc/en-us/articles/360012076960-Where-are-the-installed-cores-located-)
- ** to find where Arduino installs cores on your machine.
- **
- ** If the problem persists then there's not enough memory for this model and application.
- */
+/** Struct to link sensor axis name to sensor value function */
+typedef struct{
+    const char *name;
+    float (*get_value)(void);
+
+}eiSensors;
+
+/* Constant defines -------------------------------------------------------- */
+#define CONVERT_G_TO_MS2    9.80665f
+
+/** Number sensor axes used */
+#define NICLA_N_SENSORS     6
 
 /* Private variables ------------------------------------------------------- */
-static bool debug_nn = false; // Set this to true to see e.g. features generated from the raw signal
-static uint32_t run_inference_every_ms = 200;
-static float buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE] = { 0 };
-static float inference_buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE];
-ei_classifier_smooth_t smooth;
+static const bool debug_nn = false; // Set this to true to see e.g. features generated from the raw signal
 
-/* Forward declaration */
-void run_inference_background();
+float Ax, Ay, Az;
+float Gx, Gy, Gz;
+
+static bool ei_connect_fusion_list(const char *input_list);
+static float get_accX(void){return Ax;}
+static float get_accY(void){return Ay;}
+static float get_accZ(void){return Az;}
+static float get_gyrX(void){return Gx;}
+static float get_gyrY(void){return Gy;}
+static float get_gyrZ(void){return Gz;}
+
+static int8_t fusion_sensors[NICLA_N_SENSORS];
+static int fusion_ix = 0;
+
+/** Used sensors value function connected to label name */
+eiSensors nicla_sensors[] =
+{
+    "accX", &get_accX,
+    "accY", &get_accY,
+    "accZ", &get_accZ,
+    "gyrX", &get_gyrX,
+    "gyrY", &get_gyrY,
+    "gyrZ", &get_gyrZ,
+};
 
 /**
 * @brief      Arduino setup function
 */
 void setup()
 {
-    // put your setup code here, to run once:
+    /* Init serial */
     Serial.begin(115200);
-    Serial.println("Edge Impulse Inferencing Demo");
+    delay(3000); // Needs to wait at least 3 seconds before output works
+    Serial.println("Edge Impulse Sensor Fusion Inference\r\n");
 
-    if (!lsm6ds3trc.begin_I2C(0x6a, &Wire1)) {
+    /* Connect used sensors */
+    if(ei_connect_fusion_list(EI_CLASSIFIER_FUSION_AXES_STRING) == false) {
+        ei_printf("ERR: Errors in sensor list detected\r\n");
+        return;
+    }
+
+    /* Init & start sensors */
+    if (!lsm6ds3trc.begin_I2C()) {
         ei_printf("Failed to initialize IMU!\r\n");
     }
     else {
         ei_printf("IMU initialized\r\n");
-    }
-
-    if (EI_CLASSIFIER_RAW_SAMPLES_PER_FRAME != 6) {
-        ei_printf("ERR: EI_CLASSIFIER_RAW_SAMPLES_PER_FRAME should be equal to 6 (the 6 sensor axes)\n");
-        return;
     }
 
     // Set up Gyro and Accelerometer to match the recorded settings
@@ -81,41 +96,52 @@ void setup()
     lsm6ds3trc.setGyroDataRate(LSM6DS_RATE_104_HZ);
     
     lsm6ds3trc.setAccelRange(LSM6DS_ACCEL_RANGE_2_G);
-    lsm6ds3trc.setAccelDataRate(LSM6DS_RATE_104_HZ);
+    lsm6ds3trc.setAccelDataRate(LSM6DS_RATE_104_HZ);    
+}
 
-    // Fill up the buffer
-    for (int i=0; i<EI_CLASSIFIER_RAW_SAMPLE_COUNT; i++) {
-      measure_imu();
+/**
+* @brief      Get data and run inferencing
+*/
+void loop()
+{
+    ei_printf("\nStarting inferencing in 2 seconds...\r\n");
+
+    delay(2000);
+
+    if (EI_CLASSIFIER_RAW_SAMPLES_PER_FRAME != fusion_ix) {
+        ei_printf("ERR: Nicla sensors don't match the sensors required in the model\r\n"
+        "Following sensors are required: %s\r\n", EI_CLASSIFIER_FUSION_AXES_STRING);
+        return;
     }
 
-    // This is a structure that smoothens the output result
-    // With the default settings 70% of readings should be the same before classifying.
-    ei_classifier_smooth_init(&smooth, 10 /* no. of readings */, 7 /* min. readings the same */, 0.8 /* min. confidence */, 0.3 /* max anomaly */);
-}
+    ei_printf("Sampling...\r\n");
 
-/**
- * @brief Return the sign of the number
- * 
- * @param number 
- * @return int 1 if positive (or 0) -1 if negative
- */
-float ei_get_sign(float number) {
-    return (number >= 0.0) ? 1.0 : -1.0;
-}
+    // Allocate a buffer here for the values we'll read from the IMU
+    float buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE] = { 0 };
 
-/**
- * @brief      Run inferencing in the background.
- */
-void run_inference_background()
-{
-    // copy the buffer
-    memcpy(inference_buffer, buffer, EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE * sizeof(float));
+    for (size_t ix = 0; ix < EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE; ix += EI_CLASSIFIER_RAW_SAMPLES_PER_FRAME) {
+        // Determine the next tick (and then sleep later)
+        int64_t next_tick = (int64_t)micros() + ((int64_t)EI_CLASSIFIER_INTERVAL_MS * 1000);
+
+        // Update function should be continuously polled
+        update_sensors();
+
+        for(int i = 0; i < fusion_ix; i++) {
+            buffer[ix + i] = nicla_sensors[fusion_sensors[i]].get_value();
+        }
+
+        int64_t wait_time = next_tick - (int64_t)micros();
+
+        if(wait_time > 0) {
+            delayMicroseconds(wait_time);
+        }
+    }
 
     // Turn the raw buffer in a signal which we can the classify
     signal_t signal;
-    int err = numpy::signal_from_buffer(inference_buffer, EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE, &signal);
+    int err = numpy::signal_from_buffer(buffer, EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE, &signal);
     if (err != 0) {
-        ei_printf("Failed to create signal from buffer (%d)\n", err);
+        ei_printf("ERR:(%d)\r\n", err);
         return;
     }
 
@@ -124,70 +150,97 @@ void run_inference_background()
 
     err = run_classifier(&signal, &result, debug_nn);
     if (err != EI_IMPULSE_OK) {
-        ei_printf("ERR: Failed to run classifier (%d)\n", err);
+        ei_printf("ERR:(%d)\r\n", err);
         return;
     }
 
     // print the predictions
-    ei_printf("Predictions ");
-    ei_printf("(DSP: %d ms., Classification: %d ms., Anomaly: %d ms.)",
+    ei_printf("Predictions (DSP: %d ms., Classification: %d ms., Anomaly: %d ms.):\r\n",
         result.timing.dsp, result.timing.classification, result.timing.anomaly);
-    ei_printf(": ");
-
-    // ei_classifier_smooth_update yields the predicted label
-    const char *prediction = ei_classifier_smooth_update(&smooth, &result);
-    ei_printf("%s ", prediction);
-    // print the cumulative results
-    ei_printf(" [ ");
-    for (size_t ix = 0; ix < smooth.count_size; ix++) {
-        ei_printf("%u", smooth.count[ix]);
-        if (ix != smooth.count_size + 1) {
-            ei_printf(", ");
-        }
-        else {
-          ei_printf(" ");
-        }
+    for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++) {
+        ei_printf("%s: %.5f\r\n", result.classification[ix].label, result.classification[ix].value);
     }
-    ei_printf("]\n");
-
-    delay(run_inference_every_ms);
+#if EI_CLASSIFIER_HAS_ANOMALY == 1
+    ei_printf("    anomaly score: %.3f\r\n", result.anomaly);
+#endif
 }
 
-void measure_imu() {
-    sensors_event_t accel;
-    sensors_event_t gyro;
-    sensors_event_t temp;
-    // Determine the next tick (and then sleep later)
-    uint64_t next_tick = micros() + (EI_CLASSIFIER_INTERVAL_MS * 1000);
+#if !defined(EI_CLASSIFIER_SENSOR) || (EI_CLASSIFIER_SENSOR != EI_CLASSIFIER_SENSOR_FUSION && EI_CLASSIFIER_SENSOR != EI_CLASSIFIER_SENSOR_ACCELEROMETER)
+#error "Invalid model for current sensor"
+#endif
 
-    // roll the buffer -3 points so we can overwrite the last one
-    numpy::roll(buffer, EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE, -6);
-    lsm6ds3trc.getEvent(&accel, &gyro, &temp);
-    // read to the end of the buffer
-    buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE - 6] = accel.acceleration.x;
-    buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE - 5] = accel.acceleration.y;
-    buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE - 4] = accel.acceleration.z;
-    buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE - 3] = gyro.gyro.x;
-    buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE - 2] = gyro.gyro.y;
-    buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE - 1] = gyro.gyro.z;
 
-    // and wait for next tick
-    uint64_t time_to_wait = next_tick - micros();
-    delay((int)floor((float)time_to_wait / 1000.0f));
-    delayMicroseconds(time_to_wait % 1000);
+/**
+ * @brief Go through nicla sensor list to find matching axis name
+ *
+ * @param axis_name
+ * @return int8_t index in nicla sensor list, -1 if axis name is not found
+ */
+static int8_t ei_find_axis(char *axis_name)
+{
+    int ix;
+    for(ix = 0; ix < NICLA_N_SENSORS; ix++) {
+        if(strstr(axis_name, nicla_sensors[ix].name)) {
+            return ix;
+        }
+    }
+    return -1;
 }
 
 /**
-* @brief      Get data and run inferencing
-*
-* @param[in]  debug  Get debug info if true
-*/
-void loop()
+ * @brief Check if requested input list is valid sensor fusion, create sensor buffer
+ *
+ * @param[in]  input_list      Axes list to sample (ie. "accX + gyrY + magZ")
+ * @retval  false if invalid sensor_list
+ */
+static bool ei_connect_fusion_list(const char *input_list)
 {
-    run_inference_background();
-    measure_imu();
+    char *buff;
+    bool is_fusion = false;
+
+    /* Copy const string in heap mem */
+    char *input_string = (char *)ei_malloc(strlen(input_list) + 1);
+    if (input_string == NULL) {
+        return false;
+    }
+    memset(input_string, 0, strlen(input_list) + 1);
+    strncpy(input_string, input_list, strlen(input_list));
+
+    /* Clear fusion sensor list */
+    memset(fusion_sensors, 0, NICLA_N_SENSORS);
+    fusion_ix = 0;
+
+    buff = strtok(input_string, "+");
+
+    while (buff != NULL) { /* Run through buffer */
+        int8_t found_axis = 0;
+
+        is_fusion = false;
+        found_axis = ei_find_axis(buff);
+
+        if(found_axis >= 0) {
+            if(fusion_ix < NICLA_N_SENSORS) {
+                fusion_sensors[fusion_ix++] = found_axis;
+            }
+            is_fusion = true;
+        }
+
+        buff = strtok(NULL, "+ ");
+    }
+
+    ei_free(input_string);
+
+    return is_fusion;
 }
 
-#if !defined(EI_CLASSIFIER_SENSOR) || EI_CLASSIFIER_SENSOR != EI_CLASSIFIER_SENSOR_FUSION
-#error "Invalid model for current sensor"
-#endif
+void update_sensors() {
+    sensors_event_t accel;
+    sensors_event_t gyro;
+    sensors_event_t temp;
+    Ax = accel.acceleration.x;
+    Ay = accel.acceleration.y;
+    Az = accel.acceleration.z;
+    Gx = gyro.gyro.x;
+    Gy = gyro.gyro.y;
+    Gz = gyro.gyro.z;
+}
